785e206ea50017233832ea239f64256aec45f01f
Bob Wint <usa44011@yahoo.com>
2022-01-02 14:09:12 -0500

    Initial Commit

---
+++ GitHub Demo/GitHub_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_DemoApp.swift
+//  GitHub Demo
+//
+//  Created by Bob Wint on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
